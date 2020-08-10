98a8e750589e859a166fe0683a6c8751a1524279
Jeremy Schoffen <jschoffen.dev@gmail.com>
2020-08-10 21:26:22 +0200

    build stuff.

--- scripts/build.clj
+++ scripts/build.clj
@@ -1,17 +1,14 @@
 (ns build
   (:require
     [clojure.spec.test.alpha :as spec-test]
-    [com.jeremyschoffen.mbt.alpha.core :as mbt-core]
-    [com.jeremyschoffen.mbt.alpha.default :as mbt-defaults]))
+    [fr.jeremyschoffen.mbt.alpha.core :as mbt-core]
+    [fr.jeremyschoffen.mbt.alpha.default :as mbt-defaults]))
 
 (spec-test/instrument)
 
 
-
-
 (def specific-conf (sorted-map
                      :maven/group-id 'fr.jeremyschoffen
-                     :versioning/major :alpha
                      :versioning/scheme mbt-defaults/simple-scheme))
 
 
@@ -19,6 +16,11 @@
 
 
 (comment
+  (mbt-defaults/bump-tag! conf)
+  (mbt-defaults/build-jar! conf)
+  (mbt-defaults/install! conf)
+  (mbt-core/clean! conf)
+
   (-> conf
       (assoc :project/version "0")
       mbt-defaults/build-jar!)
