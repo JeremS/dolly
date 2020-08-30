3d6e9f97106d38cfea62d9f41c75095bbce13d18
Jeremy Schoffen <jschoffen.dev@gmail.com>
2020-08-30 20:35:25 +0200

    Added some docs and updated the build script.

---
+++ src/doc/docs/core.clj
@@ -0,0 +1,18 @@
+(ns docs.core
+  (:require
+    [fr.jeremyschoffen.textp.alpha.doc.core :as tp-doc]
+    [fr.jeremyschoffen.mbt.alpha.utils :as u]))
+
+
+(u/pseudo-nss
+  project)
+
+
+
+(def readme-path "docs/pages/README.md.tp")
+
+
+(defn make-readme! [{wd ::project/working-dir
+                     coords ::project/maven-coords}]
+  (spit (u/safer-path wd "README.md")
+        (tp-doc/make-document readme-path {:project/maven-coords coords})))
